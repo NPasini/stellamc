7e8df7daa6d60266365fe2da75b37dc9b647a55f
Nicolò Pasini <nicolo.pasini@igenius.ai>
2021-03-26 22:35:44 +0100

    Updated Product model

--- stellamc/Models/DepartmentProduct.swift
+++ stellamc/Models/DepartmentProduct.swift
@@ -1,5 +1,5 @@
 //
-//  DepartmentProduct.swift
+//  Product.swift
 //  stellamc
 //
 //  Created by Nicolò Pasini on 26/03/21.
@@ -7,6 +7,34 @@
 
 import Foundation
 
-struct DepartmentProduct: Codable {
+struct Product: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case price = "FullPrice"
+        case name = "ModelNames"
+        case id = "DefaultCode10"
+        case microCategory = "MicroCategory"
+        case macroCategory = "MacroCategory"
+    }
 
+    let id: String
+    let price: Int
+    let name: String
+    let microCategory: String?
+    let macroCategory: String?
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        price = try container.decodeIfPresent(Int.self, forKey: .price) ?? 0
+        microCategory = try container.decodeIfPresent(String.self, forKey: .microCategory)
+        macroCategory = try container.decodeIfPresent(String.self, forKey: .macroCategory)
+    }
+}
+
+extension Product: Equatable {
+    static func ==(lhs: Product, rhs: Product) -> Bool {
+        return true
+    }
 }
