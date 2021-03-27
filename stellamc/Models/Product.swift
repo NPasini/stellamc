1004d641c2c02a2e918a2083ec3f3331931bbac4
Nicolò Pasini <nicolo.pasini@igenius.ai>
2021-03-27 15:36:26 +0100

    Updated Product structure

--- stellamc/Models/Product.swift
+++ stellamc/Models/Product.swift
@@ -17,18 +17,18 @@ struct Product: Decodable {
     }
 
     let id: String
-    let price: Int
     let name: String
+    let price: Double
+    let thumbURLString: URL?
+    let pdpImagesURLs: [URL]
     let microCategory: String?
     let macroCategory: String?
-    let thumbURLString: String
-    let pdpImagesURLStrings: [String]
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
         id = try container.decode(String.self, forKey: .id)
-        price = try container.decodeIfPresent(Int.self, forKey: .price) ?? 0
+        price = try container.decodeIfPresent(Double.self, forKey: .price) ?? 0
         let productName = try container.decodeIfPresent(String.self, forKey: .name)
         microCategory = try container.decodeIfPresent(String.self, forKey: .microCategory)
         macroCategory = try container.decodeIfPresent(String.self, forKey: .macroCategory)
@@ -37,8 +37,20 @@ struct Product: Decodable {
 
         let folderId = id.prefix(2)
         let commonURL = "https://www.stellamccartney.com/\(folderId)/\(id)_"
-        thumbURLString = commonURL + "8_c.jpg"
-        pdpImagesURLStrings = [commonURL + "11_c.jpg", commonURL + "11_g.jpg", commonURL + "11_h.jpg", commonURL + "11_i.jpg", commonURL + "11_p.jpg", commonURL + "11_q.jpg", commonURL + "11_s.jpg"]
+        thumbURLString = URL(string: commonURL + "8_c.jpg")
+
+        let pdpImagesURLStrings = [commonURL + "11_c.jpg", commonURL + "11_g.jpg", commonURL + "11_h.jpg", commonURL + "11_i.jpg", commonURL + "11_p.jpg", commonURL + "11_q.jpg", commonURL + "11_s.jpg"]
+        pdpImagesURLs = pdpImagesURLStrings.compactMap({ URL(string: $0) })
+    }
+
+    init(id: String, price: Double, name: String, microCategory: String?, macroCategory: String?, thumbURLString: URL?, pdpImagesURLStrings: [URL]) {
+        self.id = id
+        self.name = name
+        self.price = price
+        self.microCategory = microCategory
+        self.macroCategory = macroCategory
+        self.thumbURLString = thumbURLString
+        self.pdpImagesURLs = pdpImagesURLStrings
     }
 }
 
