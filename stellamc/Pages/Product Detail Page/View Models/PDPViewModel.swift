05b9ecd7e44f661bb7abdb6558a60c6fa39128b8
Nicolò Pasini <nicolo.pasini@igenius.ai>
2021-03-28 17:05:13 +0200

    Updated PDPViewModel to get the product details

--- stellamc/Pages/Product Detail Page/View Models/PDPViewModel.swift
+++ stellamc/Pages/Product Detail Page/View Models/PDPViewModel.swift
@@ -5,13 +5,42 @@
 //  Created by Nicolò Pasini on 26/03/21.
 //
 
+import OSLogger
 import Foundation
+import ReactiveSwift
 
-struct PDPViewModel: ViewModel {
+final class PDPViewModel: ViewModel, ObservableObject {
 
-    let product: Product
+    @Published private(set) var product: Product
+    
+    private let serialDisposable: SerialDisposable
+    private let productDetailsRepository: ProductDetailsRepositoryService?
 
     init(product: Product) {
         self.product = product
+        self.serialDisposable = SerialDisposable()
+        self.productDetailsRepository = AssemblerWrapper.shared.resolve(ProductDetailsRepositoryService.self)
+
+        getProductDetails()
+    }
+
+    deinit {
+        OSLogger.dataFlowLog(message: "Disposing PDPViewModel", access: .public, type: .debug)
+
+        if !serialDisposable.isDisposed {
+            serialDisposable.dispose()
+        }
+    }
+
+    func getProductDetails() {
+        OSLogger.dataFlowLog(message: "Fetching Details for product id \(product.id)", access: .public, type: .debug)
+
+        if let repository = productDetailsRepository {
+            serialDisposable.inner = repository.getDetails(for: product.productCode).observe(on: UIScheduler()).on(failed: { [weak self] (error: NSError) in
+                self?.product.setDetails(nil)
+            }, value: { [weak self] (details: ProductDetails) in
+                self?.product.setDetails(details)
+            }).start()
+        }
     }
 }
